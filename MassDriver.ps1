acf8cfaff1b198132153b85dcd0903c94f324a1c
WravenDERF <90710167+WravenDERF@users.noreply.github.com>
2022-04-11 11:50:26 -0400

    Update MassDriver.ps1

--- MassDriver.ps1
+++ MassDriver.ps1
@@ -7,6 +7,54 @@ $MassDriver = [PSCustomObject]@{
 }
 
 
+Add-Member -InputObject $MassDriver -MemberType 'ScriptMethod' -Name 'FujiSynapseUninstall' -Force -Value {
+
+
+    PARAM (
+        [Parameter()][string]$ComputerName
+    )
+
+
+    Clear-Host
+    IF ($MassDriver.Debug) {Write-Host -Object $ComputerName}
+    IF ($MassDriver.Debug) {Write-Host -Object $ComputerName}
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+        #Stop the Agent Desktop if it is running.
+        IF (Get-Process -Name 'Fujifilm.Synapse.Agent.exe' -ErrorAction 'SilentlyContinue') {Stop-Process "Fujifilm.Synapse.Agent.exe" -Force}
+        #Uninstall the HTML5 TWAIN Web component.
+        Start-Process -FilePath 'C:\Windows\system32\MSIEXEC.EXE' -ArgumentList '/x {7BF08D44-2176-4A7B-A8B7-8E4DC7424D5D} /qb-' -Wait
+        #Uninstall the Installation Helper 1.3.1.0 component.
+        Start-Process -FilePath 'C:\Windows\system32\MSIEXEC.EXE' -ArgumentList '/x {C21DF62B-3850-4977-8061-AD346FB14883} /qb-' -Wait
+        #Uninstall the MPR Fusion component.
+        Start-Process -FilePath 'C:\Windows\system32\MSIEXEC.EXE' -ArgumentList '/x {B6729277-10DA-4A2E-BABA-6B50002C5E57} /qb-' -Wait
+        
+        #Uninstall the Fuji Synapse 4.4.200.
+        Start-Process -FilePath 'C:\Windows\system32\MSIEXEC.EXE' -ArgumentList '/x {C3FE4D19-8346-466A-B3EF-6A13867E8FDD} /qb-' -Wait
+        
+        #Uninstall the Fuji Synapse 5.5 Desktop Agent.
+        Start-Process -FilePath 'C:\Windows\system32\MSIEXEC.EXE' -ArgumentList '/x {C3FE4D19-8346-466A-B3EF-6A13867E8FDD} /qb-' -Wait
+        #Uninstall the Fuji Synapse 5.7.200 Desktop Agent.
+        Start-Process -FilePath 'C:\Windows\system32\MSIEXEC.EXE' -ArgumentList '/x {64D3590F-3BF8-4E61-994F-9AFB89EA6176} /qb-' -Wait
+        Start-Process -FilePath 'C:\Windows\system32\MSIEXEC.EXE' -ArgumentList '/x {56839E35-532F-479D-8BB9-64D3546DF819} /qb-' -Wait
+        #Uninstall the Fuji Synapse 5.7.220 Desktop Agent.
+        Start-Process -FilePath 'C:\Windows\system32\MSIEXEC.EXE' -ArgumentList '/x {E6EB995F-F572-4DB9-A61A-0C3E6D11F75F} /qb-' -Wait
+        #Uninstall documentation links.
+        IF (Test-Path -Path "$Env:PUBLIC\Desktop\Fuji Synapse 5 Training.lnk") {Remove-Item -Path "$Env:PUBLIC\Desktop\Fuji Synapse 5 Training.lnk"}
+        #Uninstall Content Manager icon.
+        IF (Test-Path -Path "$Env:PUBLIC\Desktop\Content Manager.lnk") {Remove-Item -Path "$Env:PUBLIC\Desktop\Content Manager.lnk"}
+        #Uninstall old tools.
+        IF (Test-Path -Path "$Env:PUBLIC\Desktop\ScanKill.lnk") {Remove-Item -Path "$Env:PUBLIC\Desktop\ScanKill.lnk"}
+        #Uninstall Fuji Web Icon.
+        IF (Test-Path -Path "$Env:PUBLIC\Desktop\Synapse 5 Web.lnk") {Remove-Item -Path "$Env:PUBLIC\Desktop\Synapse 5 Web.lnk"}
+        #Remove registry keys.
+        IF (Test-Path -Path "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Fuji Synapse 5 Web") {Remove-Item -Path "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Fuji Synapse 5 Web"}
+        #Remove Scheduled Task for PS360 Logout.
+        Start-Process -FilePath 'C:\Windows\system32\SCHTASKS.EXE' -ArgumentList /DELETE /F /TN "Piedmont\LOGOUT-FUJISYNAPSE" -Wait
+    }
+
+}
+
+
 Add-Member -InputObject $MassDriver -MemberType 'ScriptMethod' -Name 'FujiSynapseInstallAgentPROD' -Force -Value {
 
 
